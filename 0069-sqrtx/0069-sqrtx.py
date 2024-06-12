5c23e9f9a8d93d6686c021ae70163f826cea1246
LetsBe-Anonymus <84140293+LetsBe-Anonymus@users.noreply.github.com>
2024-06-12 23:38:30 +0500

    Time: 45 ms (34.09%), Space: 16.5 MB (32.14%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,22 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 1:
+            return 1
+        if x == 0:
+            return 0
+        left = 1 
+        right = x
+
+        while left <= right:
+            mid = left + (right-left) // 2
+            pot = mid*mid
+            if pot == x:
+                return mid
+            elif pot > x:
+                right = mid - 1
+            elif pot < x:
+                left = mid + 1
+
+        return right
+
+
